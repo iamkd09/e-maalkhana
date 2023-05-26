e57323df11f988d89a0b0cc8aafea6f00b10679a
root <kanishk.dixit@myparkplus.com>
2023-05-26 10:29:27 +0530

    scrap

--- search.php
+++ search.php
@@ -109,8 +109,12 @@ if (isset($_POST['gd_search']) && !empty($_POST['gd_search'])) {
                         echo '</div></div></div></div>';
                      }
                   } else {
-                     echo '<img src="assets/img/nodatapolice.jpeg" width="50%" alt="" srcset="" style="margin-left: 23%;"/>';
+                     echo '<div class="card custom-card col-sm-12 col-md-5">
+                     <div class="">
+                     <div class="my-card">';
+                     echo '<img src="assets/img/nodatapolice.jpeg" width="35%" alt="" srcset="" style="margin-left: 32%;"/>';
                      echo '<h3 style="text-align: center;">' . $lang['no_data'] . '!</h3>';
+                     echo '</div></div></div>';
                   }
 
                   function getFieldLabel($fieldName) {
